a1a047d712b0e8142d8993c8f860d9ead3c110d2
pupukusanJP <pupuku193@gmail.com>
2025-05-05 13:19:27 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
