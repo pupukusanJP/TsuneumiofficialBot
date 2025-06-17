35c26a00549be7346e75df915a39ceef322a19ab
pupukusanJP <pupuku193@gmail.com>
2025-06-18 00:00:01 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -56,9 +56,9 @@ def home():
     return "ボットは稼働中です！"
 
 # Webサーバーを別スレッドで実行
-def run():
-    port = int(os.environ.get("PORT", 8080))  # PORT環境変数がRenderで自動的に設定される
-    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+      port = int(os.environ.get("PORT", 8000))
+      uvicorn.run(app, host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
